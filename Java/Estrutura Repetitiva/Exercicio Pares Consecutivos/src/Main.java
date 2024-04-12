5e51a707455330ffb9eb2f43389db84415e5087a
Luis Felipe <94643466+LuisJunghans@users.noreply.github.com>
2024-04-12 00:58:48 -0300

    Add files via upload

--- Java/Estrutura Repetitiva/Exercicio Pares Consecutivos/src/Main.java
+++ Java/Estrutura Repetitiva/Exercicio Pares Consecutivos/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+			int soma = 0;
+			
+			System.out.print("Digite um numero inteiro: ");
+			int numero = scan.nextInt();
+			
+			while (numero != 0) {
+				if (numero % 2 == 0) {
+					soma = numero * 5 + 20; 
+				} else if (numero % 2 != 0) {
+					soma = numero * 5 + 25;
+				}
+				System.out.println("SOMA = " + soma);
+				System.out.print("Digite um numero inteiro: ");
+				numero = scan.nextInt();
+			}
+		scan.close();
 	}
 
 }
